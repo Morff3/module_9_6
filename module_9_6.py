835180f811f0c75256125b680664a81c70c3c468
Morff3 <166973084+Morff3@users.noreply.github.com>
2024-07-11 23:20:27 +0700

    Update module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,14 +1,9 @@
 def all_variants(text):
-    i = 0
-    while i != len(text):
-        yield text[i]
-        i += 1
-    yield text[0] + text[1]
-    yield text[1] + text[2]
-    yield text[0] + text[1] + text[2]
+    for i in range(len(text)):
+        for j in range(len(text) - i):
+            yield text[j:j + i + 1]
 
 
 a = all_variants("abc")
-
 for i in a:
     print(i)
